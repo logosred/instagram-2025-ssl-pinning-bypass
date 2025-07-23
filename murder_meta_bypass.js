f9cbb0eb66b0482cd30737615f7db95ff0e17725
Cătă <thedamiancruz@gmail.com>
2025-07-23 16:45:18 +0300

    Murder Meta Bypass

---
+++ murder_meta_bypass.js
@@ -0,0 +1,25 @@
+// https://github.com/log-cat/murder-meta-bypass
+// Simple script to bypass SSL pinning in Instagram.
+Java.perform(function() {
+    console.log("--- Murder Meta Bypass Loaded ---");
+    console.log("--- Targeting the core 'verify' method ---");
+
+    try {
+        const CertificateVerifier = Java.use("com.facebook.mobilenetwork.internal.certificateverifier.CertificateVerifier");
+
+        CertificateVerifier.verify.overload(
+            '[Ljava.security.cert.X509Certificate;', 
+            'java.lang.String', 
+            'boolean'
+        ).implementation = function(certChain, hostname, someBoolean) {
+            console.log(`[+] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean}). Certificate chain is now trusted.`);
+            return;
+        };
+
+        console.log("[+] Hook on CertificateVerifier.verify with correct signature is active.");
+
+    } catch (e) {
+        console.error("[-] Failed to hook CertificateVerifier.verify(). Error:");
+        console.error(e);
+    }
+});
